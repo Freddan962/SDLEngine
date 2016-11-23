0008e058a5746a81f46cef0197c21db22d70570f
Fredrik Sander <fredde.sander@gmail.com>
2016-11-23 21:42:49 +0100

    Code cleanup

--- ImageLoader.cpp
+++ ImageLoader.cpp
@@ -8,10 +8,6 @@ ImageLoader::ImageLoader(SDL_Renderer* renderer)
 
 SDL_Texture* ImageLoader::loadBMP(std::string path)
 {
-
-	std::ifstream infile(path);
-	bool good = infile.good();
-
 	SDL_Surface* surface = SDL_LoadBMP(path.c_str());
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(mRenderer, surface);
 	SDL_FreeSurface(surface);
